479bbf1ad331197114fedbec56144536ef4c6ff1
turbokuzmich <kurteev.d@yandex.ru>
2024-07-04 18:19:34 +0300

    fix db config

--- src/config/index.ts
+++ src/config/index.ts
@@ -31,7 +31,7 @@ export default async function loadConfig(): Promise<Config> {
       },
       production: {
         dialect: 'postgres',
-        database: 'ses',
+        database: process.env.DB_DATABASE,
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
